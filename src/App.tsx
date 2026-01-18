a590c8b90bbf8a592a64030d98106f718b351a2a
antodev <antoniocastizo99@gmail.com>
2026-01-18 15:27:23 +0100

    stack

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { useState, useEffect } from 'react'
 import './index.css'
 import Header from './components/Header'
+import Stack from './components/Stack'
 
 export default function App() {
 
   return (
     <>
       <Header />
+      <Stack /> 
     </>
   )
 }
