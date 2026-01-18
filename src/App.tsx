57f8b649f928169b8f6a0ead6ab6f8f1868bbef5
antodev <antoniocastizo99@gmail.com>
2026-01-18 12:53:21 +0100

    Presentación

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react'
 import './index.css'
+import Header from './components/Header'
 
 export default function App() {
 
   return (
     <>
-      
+      <Header />
     </>
   )
 }
